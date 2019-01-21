7c1bf389384ee75bb3d641c49b2eaf756b0f3376
han057 <fredyhernan057@gmail.com>
2019-01-21 22:12:05 +0100

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='dswp',
+      version='1.0',
+      description='Data science and Web programming(Big data)',
+      author='Lucia Pascarella, Ilaria Avino, Fabio Jimenez, Fredy Sanchez, ',
+      author_email='fredyhernan057@gmail.com',
+      url='https://github.com/han057/data_science_web_programming',
+      install_requires=['tweepy', 'pysolr'],
+     )
